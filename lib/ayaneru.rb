513b5a28145a34ecb660b045e108130c50e87293
Tomohiro Suwa <neoen.gsn@gmail.com>
2013-09-01 23:28:57 +0900

    store reservation tags in redis

--- lib/ayaneru.rb
+++ lib/ayaneru.rb
@@ -1,8 +1,15 @@
 require 'ayaneru/server'
 require 'ayaneru/niconico'
+require 'redis'
 
 module Ayaneru
 	def self.niconico
-		@niconico = Niconico.instance
+		@niconico ||= Niconico.instance
+	end
+
+	def self.redis
+		@redis ||= Redis.connect
+		@redis.client.reconnect unless @redis.client.connected?
+		@redis
 	end
 end
--- lib/ayaneru/server.rb
+++ lib/ayaneru/server.rb
@@ -8,10 +8,15 @@ module Ayaneru
 		end
 
 		get '/schedule' do
+			@registered_tags = Ayaneru.redis.lrange "tags", 0, -1
 			result = Ayaneru.niconico.search.to_s.split("\n")
 			@json = JSON.parse(result[2])
-
 			haml :schedule
+		end
+
+		post '/schedule' do
+			Ayaneru.redis.rpush "tags", params[:tag]
+			redirect '/schedule'
 		end
 	end
 end
