9af4527843c4211acaf413450962bcd143c89ee2
Tomohiro Suwa <neoen.gsn@gmail.com>
2013-09-04 04:10:39 +0900

    using colon and each

--- lib/ayaneru/server.rb
+++ lib/ayaneru/server.rb
@@ -8,7 +8,7 @@ module Ayaneru
 		end
 
 		get '/schedule' do
-			@registered_tags = Ayaneru.redis.lrange "tags", 0, -1
+			@registered_tags = Ayaneru.redis.lrange 'tags', 0, -1
 			@results = {}
 			@registered_tags.each do |tag|
 				r = Ayaneru.niconico.search(tag, 7).to_s.split("\n")
@@ -24,14 +24,14 @@ module Ayaneru
 		end
 
 		get '/reserve' do
-			@registered_tags = Ayaneru.redis.lrange "tags", 0, -1
+			@registered_tags = Ayaneru.redis.lrange 'tags', 0, -1
 			@registered_tags.each do |tag|
 				r = Ayaneru.niconico.search(tag, 1).to_s.split("\n")
 				@results = JSON.parse(r[2])
-				if @results["values"]
-					@results["values"].each do |value|
-						Ayaneru.niconico.reserve(value['cmsid'])
-						Ayaneru.twitter.update "@tsuwatch 「#{value['title']}」（http://live.nicovideo.jp/watch/#{value['cmsid']}）をタイムシフト予約しました．"
+				if @results[:values]
+					@results[:values].each do |value|
+						Ayaneru.niconico.reserve(value[:cmsid])
+						Ayaneru.twitter.update "@tsuwatch 『#{value[:title]}』（http://live.nicovideo.jp/watch/#{value[:cmsid]}）をタイムシフト予約しました．"
 					end
 				end
 			end
