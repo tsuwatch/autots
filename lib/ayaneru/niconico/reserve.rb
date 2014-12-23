9936267cdd6846f8659f75c3251d065cdeda0323
Tomohiro Suwa <neoen.gsn@gmail.com>
2014-12-24 00:43:46 +0900

    Adjust indent

--- lib/ayaneru/niconico/reserve.rb
+++ lib/ayaneru/niconico/reserve.rb
@@ -1,39 +1,39 @@
 module Ayaneru
-	class Niconico
-		def reserve(lv)
-			login unless @logined
-			lv = lv.sub(/^lv/, "")
-			ulck = get_ulck(lv)
-		  do_reservation(lv, ulck)
+  class Niconico
+    def reserve(lv)
+      login unless @logined
+      lv = lv.sub(/^lv/, "")
+      ulck = get_ulck(lv)
+      do_reservation(lv, ulck)
       true
-		end
+    end
 
-		def get_ulck(lv)
-			login unless @logined
+    def get_ulck(lv)
+      login unless @logined
 
-			query = {
-				'mode' => 'watch_num',
-				'vid' => lv,
-				'token' => "watch_modal_0_official_lv#{lv}_comingsoon"
-			}
-			response = Ayaneru.niconico.agent.get(URL[:reserve], query)
+      query = {
+        'mode' => 'watch_num',
+        'vid' => lv,
+        'token' => "watch_modal_0_official_lv#{lv}_comingsoon"
+      }
+      response = Ayaneru.niconico.agent.get(URL[:reserve], query)
       raise UlckParseError, 'It is the limit of the number of your reservation' unless response.at('div.reserve')
-			response.at('div.reserve').inner_html.scan(/ulck_[0-9]+/)[0]
-		end
+      response.at('div.reserve').inner_html.scan(/ulck_[0-9]+/)[0]
+    end
 
-		def do_reservation(lv, ulck)
-			login unless @logined
+    def do_reservation(lv, ulck)
+      login unless @logined
 
-			post_data = {
-				'mode' => 'regist',
-				'vid' => lv,
-				'token' => ulck,
-				'_' => ''
-			}
+      post_data = {
+        'mode' => 'regist',
+        'vid' => lv,
+        'token' => ulck,
+        '_' => ''
+      }
 
-			Ayaneru.niconico.agent.post(URL[:reserve], post_data)
-		end
-	end
+      Ayaneru.niconico.agent.post(URL[:reserve], post_data)
+    end
+  end
   class UlckParseError < StandardError; end
 end
 
