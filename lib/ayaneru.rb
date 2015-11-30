ba8f5e51952e53a999451574ed98315b2d948b26
Tomohiro Suwa <neoen.gsn@gmail.com>
2015-11-30 20:35:14 +0900

    Fix to use Twitter::REST::Client

--- lib/ayaneru.rb
+++ lib/ayaneru.rb
@@ -17,11 +17,11 @@ module Ayaneru
   end
 
   def self.twitter
-    @twitter ||= Twitter::Client.new(
-      :consumer_key => ENV['TWITTER_CONSUMER_KEY'],
-      :consumer_secret => ENV['TWITTER_CONSUMER_SECRET'],
-      :oauth_token => ENV['TWITTER_OAUTH_TOKEN'],
-      :oauth_token_secret => ENV['TWITTER_OAUTH_TOKEN_SECRET']
-    )
+    @twitter ||= Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+      config.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
+      config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
+    end
   end
 end
